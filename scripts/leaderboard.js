1e4b31e3bd97ee8b3b49d5cf32aa6639a3eef75f
benjamin <benjamin.biollaz@students.hevs.ch>
2022-05-24 23:23:03 +0200

    added for of loop to fulfill criteria

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -3,14 +3,15 @@ document.body.onload = init;
 
 function init() {
     let ldbStorage = JSON.parse(localStorage.getItem("leaderBoard"));
-    for (let i = 0; i < ldbStorage.leaderBoard.length; i++) {
-        let boardArray = ldbStorage.leaderBoard;
-       if (boardArray[i].score > 0) {
+    let i=0;
+    for (let b of ldbStorage.leaderBoard) {
+       if (b.score > 0) {
            const p = document.createElement("p");
-           const content = document.createTextNode((i+1) + ". " + boardArray[i].userName + " : " + boardArray[i].score)
+           const content = document.createTextNode((i+1) + ". " + b.userName + " : " + b.score)
             p.appendChild(content);
            let mainDiv = document.getElementById("leaderBoard_div");
            mainDiv.appendChild(p);
+           i++;
        }
     }
 }
