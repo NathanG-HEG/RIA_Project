5c6e9f69a9b15da803a11d0ba70fe08358701d4a
benjamin <benjamin.biollaz@students.hevs.ch>
2022-05-25 14:37:28 +0200

    ugly layout buttons

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,4 +2,9 @@ function saveUserName() {
     let userName = document.getElementById("userName").value;
     userName = userName.replace(/\s/g, ''); //get rid of spaces
     localStorage.setItem("userName", userName);
+    location.replace("choix_map.html")
+}
+
+function howToPlay() {
+    location.replace("how_to_play.html")
 }
