3f10d17f0cc53501a75f3344c5aa2b2620a023d5
benjamin <benjamin.biollaz@students.hevs.ch>
2022-05-30 20:31:52 +0200

    changed leaderboard font used placholder instead of plain text javascript input validation

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,9 @@
 function saveUserName() {
     let userName = document.getElementById("userName").value;
+    if (userName == "") {
+        alert('Choose your username before playing.')
+        return;
+    }
     userName = userName.replace(/\s/g, ''); //get rid of spaces
     localStorage.setItem("userName", userName);
     location.replace("choix_map.html")
